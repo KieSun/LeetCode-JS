a4c87c7eb5833c6c18c31a92e59fab5889335358
yck <zx597813039@gmail.com>
2020-05-16 21:18:54 +0800

    [142] 环形链表 II

---
+++ 142.环形链表-ii.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    while (head && head.next) {
+        if (head.flag) {
+            return head
+        } else {
+            head.flag = 1
+            head = head.next
+        }
+    }
+    return null
+};
+// @lc code=end
+
