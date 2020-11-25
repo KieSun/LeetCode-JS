6349101f73b4a8c27b52f0e4e1f43247ba8c4d76
yck <zx597813039@gmail.com>
2020-11-25 22:18:57 +0800

    feat: 111

---
+++ 树/111.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if (!root) return 0
+    if (!root.left && !root.right) return 1
+    if (!root.left) {
+        return minDepth(root.right) + 1
+    }
+    if (!root.right) {
+        return minDepth(root.left) + 1
+    }
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+};
