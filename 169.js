ce0c7d944f908c013153981a5d2aff9bf6696a2e
yck <zx597813039@gmail.com>
2020-11-30 21:10:02 +0800

    feat: 169

---
+++ 169.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    // 摩尔投票法
+    let num = nums[0]
+    let count = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === num) {
+            count++
+        } else if (--count === 0) {
+            num = nums[i + 1]
+        }
+    }
+    return num
+};
