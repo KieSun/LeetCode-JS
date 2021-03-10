08aa49e77d5dc697dd5528fdbfd06068b656024c
yck <zx597813039@gmail.com>
2021-03-10 22:08:12 +0800

    feat: update

--- 现在刷的/lagou/栈/84.js
+++ 现在刷的/lagou/栈/84.js
@@ -9,6 +9,9 @@ var largestRectangleArea = function(heights) {
     heights = [0, ...heights, 0]
     for(let i = 0; i < heights.length; i++) {
         const v = heights[i]
+        // 找到比栈中短的木板
+        // 然后将最后一块木板索引 pop 出来用于计算
+        // 乘以当前索引 - 栈顶索引
         while (stack.length && v < height[stack[stack.length - 1]]) {
             const index = stack.pop()
             res = Math.max(res, heights[index] * (i - stack[stack.length - 1] - 1))
