18bec6ca92f470d20e9d942bedfd83561237c6dc
xuwu <zx597813039@gmail.com>
2021-07-13 22:26:24 +0800

    feat: 206

---
+++ 链表/206.反转链表.ts
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function reverseList(head: ListNode | null): ListNode | null {
+    let dummy = new ListNode()
+    while (head) {
+        let temp = head.next
+        head.next = dummy.next
+        dummy.next = head
+        head = temp
+    }
+    return dummy.next
+};
