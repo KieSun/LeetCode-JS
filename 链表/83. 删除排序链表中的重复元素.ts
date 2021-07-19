32988ffc938c2e4fc257c3fd7543e2073a507b53
xuwu <zx597813039@gmail.com>
2021-07-19 21:16:29 +0800

    feat: update

---
+++ 链表/83. 删除排序链表中的重复元素.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+ function deleteDuplicates(head: ListNode | null): ListNode | null {
+    if (!head) return head
+    let current = head
+    while(current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
+        }
+    }
+    return head
+};
